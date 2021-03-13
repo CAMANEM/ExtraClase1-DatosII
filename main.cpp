d44f0ca98bd7d2d85fe99b87e40b9f60160b9a0f
CAMANEM <camanem10@hotmail.com>
2021-03-12 22:30:17 -0600

    simple menu interface

--- main.cpp
+++ main.cpp
@@ -6,6 +6,51 @@ using namespace std;
 int main() {
 
     LinkedList* linkedList = new LinkedList();
+    int userInput;
+    bool flag = true;
+
+    while (flag){
+        cout << "------------------------------------------------------" << endl;
+        cout << " --> Enter 1 to add a node to the linked list" << endl;
+        cout << " --> Enter 2 to Delete a Node from the linked list" << endl;
+        cout << " --> Enter 3 to print the references of the linked list" << endl;
+        cout << " --> Enter 4 to print the values of the linked list and the collector" << endl;
+        cout << " --> Enter 5 to exit" << endl;
+        cout << "------------------------------------------------------" << endl;
+        cin>> userInput;
+
+        switch (userInput) {
+            case 1:
+                cout << "Enter a int value for the node" << endl;
+                cin>> userInput;
+                linkedList->newNode(userInput);
+                userInput = 0;
+                break;
+
+            case 2:
+                cout << "Enter the position (int) of the node that you want to delete" << endl;
+                cin>> userInput;
+                linkedList->deleteNode(userInput);
+                userInput = 0;
+                break;
+
+            case 3:
+                linkedList->printReferences();
+                break;
+
+            case 4:
+                linkedList->printList();
+
+            case 5:
+                flag = false;
+                break;
+
+            default:
+                break;
+        }
+
+    }
+    /*
     linkedList->newNode(10);
     linkedList->newNode(20);
     linkedList->newNode(30);
@@ -21,7 +66,7 @@ int main() {
     //linkedList->newNode(123123);
     linkedList->printReferences();
     linkedList->printList();
-
+*/
 
     return 0;
 }
